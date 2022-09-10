4009eff1a396e51835d3cd63b45b85e91f7524c6
Richard Greville <richard.greville@opticoreit.com>
2022-09-10 06:58:47 +0100

    init

--- detect.py
+++ detect.py
@@ -1,10 +1,17 @@
 import RPi.GPIO as GPIO
 import time
+import csv
 from datetime import datetime
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)
+
+column_name = ["DateTime", "Sensor"] 
+
+with open('./logs/detect.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(column_name) 
 
 while True:
     i=GPIO.input(11)
@@ -12,4 +19,9 @@ while True:
         time.sleep(0.1)
     elif i==1:
         print("motion detected @",datetime.now().strftime("%H:%M:%S"))
-        time.sleep(30)
+        with open('./logs/detect.csv', 'a') as f:
+            writer = csv.writer(f)
+            #writer.writerow(column_name) 
+            data = [datetime.now().strftime("%H:%M:%S"),"Sensor1"]
+            writer.writerow(data)
+        time.sleep(0.1)
